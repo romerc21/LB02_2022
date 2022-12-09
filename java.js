4a26a167c3bb8c90d54326928631800e27717932
zkolnl <101105382+zkolnl@users.noreply.github.com>
2022-12-09 09:51:11 +0100

    let

--- java.js
+++ java.js
@@ -1,6 +1,7 @@
+
 function validate() {
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
     if (username=="admin"&& password=="user")
     {
         alert("login succesfully");
@@ -8,7 +9,6 @@ function validate() {
 
     }
 
-}
 else
 
 {
