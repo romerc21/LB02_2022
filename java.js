305d48f109cee1cf16d7b931afc6a3db48d83a43
zkolnl <101105382+zkolnl@users.noreply.github.com>
2023-01-03 09:32:22 +0100

    feld muss ausgefüllt werden

--- java.js
+++ java.js
@@ -2,7 +2,7 @@
 function validate() {
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
-    if (username=="admin"&& password=="user")
+    // if (username=="admin"&& password=="user")
     {
         alert("login succesfully");
         return false;
@@ -16,3 +16,29 @@ else
 }
 
 }
+
+const username = document.getElementById('username')
+const password = document.getElementById('password')
+const form = document.getElementById('form')
+const errorElement = document.getElementById('error')
+
+form.addEventListener('submit' ,(e) => {
+    let messages = []
+    if (username.value === '' || username.value == null) {
+        messages.push('Username is required')
+    }
+
+    if (password.value.length <= 6) {
+        messages.push('password must be longer than 6 characters')
+    }
+
+    if (password.value.length >= 20) {
+        messages.push('password must be less than 20 characters')
+    }
+
+
+    if (messages.length > 0) {
+        e.preventDefault()
+        errorElement.innerText= messages.join(', ')
+    }
+})
